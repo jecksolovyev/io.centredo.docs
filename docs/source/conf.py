4971e947bdd9b9162ef56847da4f05ac6f3cf389
Vasyl Martynov <vm@whitedoc.software>
2023-01-17 13:33:14 +0200

    Added info on links and expanded structured docs documents

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2022, WhiteDoc'
 author = 'WhiteDoc'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.27'
 
 
 # -- General configuration ---------------------------------------------------
